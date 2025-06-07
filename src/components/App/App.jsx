9eec14929bd42275ada04939bb1d621b2aa7c393
Jade <jadet480@googlemail.com>
2025-06-07 10:48:02 +0100

    Add basic structure and component files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+// import components as they are created
+
+function App() {
+ return (
+ <>
+  <h1>Jammming!</h1>
+  <div>
+    <p>SearchBar component</p>
+    <p>SearchResults component</p>
+    <p>Playlist component</p>
+  </div>
+ </>
+ )
+};
+
+export default App;
