879fbd77c3ed796c4fd69ea946ae67ccce5afa84
Jade <jadet480@googlemail.com>
2025-06-07 13:58:16 +0100

    Add basic structure to SearchResults

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 
 // import components as they are created
 import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from '../SearchResults/SearchResults';
 
 function App() {
  return (
@@ -10,7 +11,7 @@ function App() {
   <h1>Jammming!</h1>
   <div>
     <SearchBar />
-    <p>SearchResults component</p>
+    <SearchResults />
     <p>Playlist component</p>
   </div>
  </>
---
+++ src/components/SearchResults/SearchResults.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchResults = (props) => {
+  return (
+    <>
+      <h2>Search Results</h2>
+      <p>Tracklist component</p>
+    </>
+  )
+};
+
+export default SearchResults;
